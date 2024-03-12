fe63e1716bd5a8e0ff675e7fe0f7d04261acbbca
CristoSparda <cristo.n6-@hotmail.com>
2024-03-12 14:14:05 -0600

    inicio de los cambios en el css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -16,7 +17,43 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
+        
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            blue: {
+              light: '#85d7ff',
+              DEFAULT: '#1fb6ff',
+              dark: '#009eeb',
+            },
+            pink: {
+              light: '#ff7ce5',
+              DEFAULT: '#ff49db',
+              dark: '#ff16d1',
+            },
+            gray: {
+              darkest: '#1f2d3d',
+              dark: '#3c4858',
+              DEFAULT: '#c0ccda',
+              light: '#e0e6ed',
+              lightest: '#f9fafc',
+            },
+            black: {
+                DEFAULT: '#232323',
+            }
+        },
+
+        textColor: {
+            primary: '#FFFFFF',
+            secondary: '#000000',
+            innactive: '#B3B3B3',
+        },
+      
+       
     },
 
     plugins: [forms],
+
+    
+
 };
